375dd9ecd720825734d598c4d3b4cab39661d6ad
Adam Kang <adamkang95@gmail.com>
2019-10-13 17:58:27 -0700

    Updates

---
+++ precache-manifest.4eb53068e208de0cdfa70d82b3a78015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1327bdfd1e1df817df58048b4c1399",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "21bc7a9fd86197a8fb33",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b07af8ad4bccc77b01f0",
+    "url": "/robofriends/static/css/main.80eeecc5.chunk.css"
+  },
+  {
+    "revision": "21bc7a9fd86197a8fb33",
+    "url": "/robofriends/static/js/2.81c81dda.chunk.js"
+  },
+  {
+    "revision": "b07af8ad4bccc77b01f0",
+    "url": "/robofriends/static/js/main.19e5bd06.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  }
+]);
